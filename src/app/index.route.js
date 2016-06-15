21c13150a96269461d175b6febffed3bf7c91f9d
Andrii Ovdiienko <Andrii.Ovdiienko@medhost.com>
2016-06-15 18:35:33 +0300

    added routing separation for mobile and desktop

--- src/app/index.route.js
+++ src/app/index.route.js
@@ -9,18 +9,50 @@
   function routerConfig($stateProvider, $urlRouterProvider) {
     $stateProvider
       .state('root', {
+        abstract: true        
+      })
+
+      // DESKTOP STATES
+
+      .state('root.desktop', {
         abstract: true
       })
+      .state('root.desktop.home', {
+        url: '/',
+        views: {
+          '@': {
+            controller: 'DesktopHomeController',
+            controllerAs: 'home',
+            templateUrl: 'app/desktop/home/desktop.home.html'
+          }
+        },
+        resolve: {
+          isMobile: ['$window', function isMobile($window) {
+            return $window.document.body.clientWidth <= 767;
+          }]
+        },
+        onEnter: ['$state', 'isMobile', function onEnter($state, isMobile) {
+          console.log('isMobile: ' + isMobile);
+          if (isMobile) {
+            $state.go('root.mobile.home');
+          }
+        }]      
+      })
+
+
+      // MOBILE STATES
+
       .state('root.mobile', {
-        abstract: true
+        abstract: true,
+        url: '/mobile'
       })
       .state('root.mobile.home', {
-        url: '/',
+        url: '/home',
         views: {
           '@': {
             controller: 'MobileHomeController',
             controllerAs: 'mobileHome',
-            templateUrl: 'app/mobile/home/home.html'
+            templateUrl: 'app/mobile/home/mobile.home.html'
           }
         }        
       });
